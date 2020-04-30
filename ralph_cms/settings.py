b1dcb2a0e88b66533abf137cf71a92d40c39e52c
Daniil <dadyarri@gmail.com>
2020-04-30 17:10:33 +0300

    Add ngrok as allowed host.

--- ralph_cms/settings.py
+++ ralph_cms/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = os.environ["SECRET_KEY"]
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = bool(os.environ.get("DJANGO_DEBUG", True))
-ALLOWED_HOSTS = ["127.0.0.1", "ralph-cms.herokuapp.com"]
+ALLOWED_HOSTS = ["127.0.0.1", "ralph-cms.herokuapp.com", ".ngrok.io"]
 
 
 # Application definition
